531b87da58b986be391de7a68126a01bed027df0
Florian Lagg <kontakt@florian.lagg.at>
2016-07-31 18:49:46 +0200

    * update check_clickatell_balance for new nagiosplugin API

--- src/check_clickatell_balance.py
+++ src/check_clickatell_balance.py
@@ -9,6 +9,7 @@
 #uses nagiosplugin under Zope Public License (ZPL)
 #see http://packages.python.org/nagiosplugin/
 
+import argparse
 import nagiosplugin
 import urllib, urllib2
 
@@ -16,37 +17,16 @@ import urllib, urllib2
     EXAMPLE USE:
     python check_clickatell_balance.py -w 50 -c 20 -a APP_ID -u USER -p PASSWORD
 """
-class CBalance(nagiosplugin.Check):
-    name = 'clickatell balance checker'
-    version = '0.1'
+class Clickatell_balance(nagiosplugin.Resource):
+    def __init__(self, api, user, passwd):
+        self.api_id = api
+        self.user = user
+        self.passwd = passwd
+        
+        super(nagiosplugin.Resource, self).__init__()
 
-    def __init__(self, optparser, logger):
-        optparser.description = 'Check the balance on Clickatell.com SMS gateway using HTTP-API'
-        optparser.version = '0.1'
-        optparser.add_option(
-            '-w', '--warning', default='50', metavar='RANGE',
-            help='warning threshold (default: %default%)')
-        optparser.add_option(
-            '-c', '--critical', default='25', metavar='RANGE',
-            help='critical threshold (default: %default%)')
-        optparser.add_option(
-            '-a', '--api', default='', metavar='STRING',
-            help='api-id for clickatell')
-        optparser.add_option(
-            '-u', '--user', default='', metavar='STRING',
-            help='user for clickatell')
-        optparser.add_option(
-            '-p', '--passwd', default='', metavar='STRING',
-            help='password for clickatell')
-
-    def process_args(self, options, args):
-        self.warning = "@" + options.warning
-        self.critical = "@" + options.critical
-        self.api_id = options.api
-        self.user = options.user
-        self.passwd = options.passwd
-
-    def obtain_data(self):
+    def probe(self):
+        self.credits = None
         #prepare http request
         url = 'http://api.clickatell.com/http/getbalance'
         values = {
@@ -60,19 +40,64 @@ class CBalance(nagiosplugin.Check):
             resp_obj = urllib2.urlopen(req)
             response = resp_obj.read()
         except Exception, e:
-            return "ERR: cannot get balance, %s" % (repr(e),)
+            return nagiosplugin.Metric('balance', self.credits, )
         #read
         preOK='Credit: '
         if response.startswith(preOK):
             self.credits = float( response.lstrip(preOK) )
-            #performance data
-            self.measures = [nagiosplugin.Measure(
-               'balance', self.credits, 'credits', self.warning, self.critical)]
+        return nagiosplugin.Metric('balance', self.credits, )
+
+class Context_num(nagiosplugin.Context):
+    def __init__(self, name, warning, critical, lower_is_better = True, fmt_metric=None, result_cls=nagiosplugin.Result):
+        self.warning = float(warning)
+        self.critical = float(critical)
+        self.lower_is_better = lower_is_better
+        super(Context_num, self).__init__(name, fmt_metric, result_cls)
+
+    def evaluate(self, metric, resource):
+        if(self.lower_is_better):
+            if metric.value >= self.critical:
+                return nagiosplugin.state.Critical
+            elif metric.value >= self.warning:
+                return nagiosplugin.state.Warn
         else:
-            #something failed
-            return "Err: %s" % (response,)
+            if metric.value <= self.warning:
+                return nagiosplugin.state.Warn
+            elif metric.value <= self.critical:
+                return nagiosplugin.state.Critical
+        return nagiosplugin.state.Ok
 
+class Clickatell_balance_Summary(nagiosplugin.Summary):
+    def problem(self, results):
+        balance = results.results[0].resource.credits
+        return "balance is low: %s" % (balance,)
 
-main = nagiosplugin.Controller(CBalance)
+@nagiosplugin.guarded
+def main():
+    argp = argparse.ArgumentParser(description=__doc__)
+    argp.add_argument(
+        '-w', '--warning', default='50', metavar='RANGE',
+        help='warning threshold (default: %default%)')
+    argp.add_argument(
+        '-c', '--critical', default='25', metavar='RANGE',
+        help='critical threshold (default: %default%)')
+    argp.add_argument(
+        '-a', '--api', default='', metavar='STRING',
+        help='clickatell api-id')
+    argp.add_argument(
+        '-u', '--user', default='', metavar='STRING',
+        help='clickatell user')
+    argp.add_argument(
+        '-p', '--passwd', default='', metavar='STRING',
+        help='clickatell password')
+    args = argp.parse_args()
+    
+    check = nagiosplugin.Check(
+        Clickatell_balance(args.api, args.user, args.passwd),
+        Context_num('balance', args.warning, args.critical, lower_is_better = False, fmt_metric='Balance: {valueunit}'),
+        Clickatell_balance_Summary()
+    )
+    check.main()
+
 if __name__ == '__main__':
     main()
